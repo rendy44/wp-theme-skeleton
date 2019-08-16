b28aceb3917e5f8039d2bf3e7dac1ea232c28c40
rendy44 <rendy.de.p@gmail.com>
2019-08-16 15:47:30 +0700

    Add helper functions

--- includes/class/class-helper.php
+++ includes/class/class-helper.php
@@ -20,6 +20,13 @@ if ( ! class_exists( '\Skeleton\Helper' ) ) {
 	class Helper {
 
 		/**
+		 * Custom prefix for postmeta and usermeta.
+		 *
+		 * @var string
+		 */
+		private static $metabox_prefix = 'skltn_';
+
+		/**
 		 * Get main column bootstrap css class width
 		 *
 		 * @return int
@@ -95,9 +102,71 @@ if ( ! class_exists( '\Skeleton\Helper' ) ) {
 		public static function save_post_meta( $post_id, $options = [] ) {
 			if ( ! empty( $options ) ) {
 				foreach ( $options as $option_key => $option_value ) {
-					update_post_meta( $post_id, $option_key, $option_value );
+					update_post_meta( $post_id, self::$metabox_prefix . $option_key, $option_value );
 				}
 			}
+		}
+
+		/**
+		 * Save multiple user meta
+		 *
+		 * @param string $user_id user id.
+		 * @param array  $options meta_key => meta_value formatterd array.
+		 */
+		public static function save_user_meta( $user_id, $options = [] ) {
+			if ( ! empty( $options ) ) {
+				foreach ( $options as $option_key => $option_value ) {
+					update_user_meta( $user_id, self::$metabox_prefix . $option_key, $option_value );
+				}
+			}
+		}
+
+		/**
+		 * Delete user meta
+		 *
+		 * @param string $user_id user id.
+		 * @param string $key     user meta key.
+		 */
+		public static function delete_user_meta( $user_id, $key ) {
+			delete_user_meta( $user_id, self::$metabox_prefix . $key );
+		}
+
+		/**
+		 * Delete user meta
+		 *
+		 * @param string $post_id post id.
+		 * @param string $key     user meta key.
+		 */
+		public static function delete_post_meta( $post_id, $key ) {
+			delete_post_meta( $post_id, self::$metabox_prefix . $key );
+		}
+
+		/**
+		 * Get user meta
+		 *
+		 * @param string $key          user meta key.
+		 * @param string $user_id      user id.
+		 * @param bool   $single_value whether the meta is single or array.
+		 *
+		 * @return mixed
+		 */
+		public static function get_user_meta( $key, $user_id, $single_value = true ) {
+			return get_user_meta( $user_id, self::$metabox_prefix . $key, $single_value );
+		}
+
+		/**
+		 * Get post meta
+		 *
+		 * @param string      $key          post meta key.
+		 * @param bool|string $post_id      post id.
+		 * @param bool        $single_value whether the meta is single or array.
+		 *
+		 * @return mixed
+		 */
+		public static function get_post_meta( $key, $post_id = false, $single_value = true ) {
+			$post_id = ! $post_id ? get_the_ID() : $post_id;
+
+			return get_post_meta( $post_id, self::$metabox_prefix . $key, $single_value );
 		}
 
 		/**
@@ -120,6 +189,18 @@ if ( ! class_exists( '\Skeleton\Helper' ) ) {
 		 */
 		public static function get( $key ) {
 			return ! empty( $_GET[ $key ] ) ? $_GET[ $key ] : false; // phpcs:ignore
+		}
+
+		/**
+		 * Get array value by its key.
+		 *
+		 * @param array  $array array object.
+		 * @param string $key   array key.
+		 *
+		 * @return bool|mixed will be returned false once aray does not have key.
+		 */
+		public static function array_val( array $array, $key ) {
+			return ! empty( $array[ $key ] ) ? $array[ $key ] : false;
 		}
 	}
 }
